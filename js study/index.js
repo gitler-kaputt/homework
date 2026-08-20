8282f8a72f019d1830e4c39186eaa83616e3e955
gitler-kaputt <dilara67d@gmail.com>
2026-08-20 16:27:14 +0600

    js study

---
+++ js study/index.js
@@ -0,0 +1,26 @@
+// Задача 1.
+const firstName = "Акакий";
+const lastName = "Акакиев";
+const isStudent = true;
+console.log(firstName, lastName, isStudent);
+
+//Задача 2.
+var age = 18;
+var currentYear = 2026;
+var birthYear = currentYear - age;
+console.log(birthYear);
+
+//Задача 3.
+const str = `Меня зовут ${firstName} ${lastName} мне ${age} лет. Я ученик курса: ${isStudent}`;
+console.log(str);
+
+//Задача 4.
+let a = `123`;
+let b = `456`;
+let c = Number(`789`);
+let d = Boolean(0);
+let e = Boolean(` `);
+let result = a + b + c + d + e;
+console.log(result);
+
+//1368false
