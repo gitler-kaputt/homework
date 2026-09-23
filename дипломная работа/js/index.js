14eb7a4c143d96474023f33436ea17f2b65372b0
gitler-kaputt <dilara67d@gmail.com>
2026-09-23 17:29:20 +0600

    Add files via upload

--- дипломная работа/js/index.js
+++ дипломная работа/js/index.js
@@ -2,6 +2,7 @@ import HeaderFixed from "./header.js";
 import BurgerMenu from "./burger.js";
 import { countDownDate } from "./timer.js";
 import { moneyAbilitiesSlider } from "./money-abilities-slider.js";
+import { updateDashboard } from "./investment-portfolio.js";
 
 try {
   const headerFixed = new HeaderFixed({
@@ -30,6 +31,7 @@ try {
 
   countDownDate();
   moneyAbilitiesSlider();
+  updateDashboard();
 } catch (error) {
   console.error(error);
 }
